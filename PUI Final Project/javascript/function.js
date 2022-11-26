f133fffb71293f543a740a21b817f7d79b429907
liansara <97773028+liansara@users.noreply.github.com>
2022-11-25 19:49:59 -0500

    progress

--- PUI Final Project/javascript/function.js
+++ PUI Final Project/javascript/function.js
@@ -4,7 +4,8 @@ $(window).scroll(function () {
 
 function parallax() {
     var wScroll = $(window).scrollTop();
-    $('.parallax--bg')
+    $('.parallax--bg').css('position', 'center ' + (wScroll) + 'px')
 }
 
 
+// trying to copy https://www.youtube.com/watch?v=SmolT-tV5Lw&t=295s
