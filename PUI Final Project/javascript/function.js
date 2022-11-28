191e76488cef2adeda2135b17962c8230b766f31
liansara <97773028+liansara@users.noreply.github.com>
2022-11-28 13:05:51 -0500

    more update on animation

--- PUI Final Project/javascript/function.js
+++ PUI Final Project/javascript/function.js
@@ -1,34 +1,56 @@
-// $(window).scroll(function () {
-//     parallax();
-// })
+//source: https://animejs.com/documentation/#cssSelector//
 
-// function parallax() {
-//     var wScroll = $(window).scrollTop();
-//     $('.parallax--bg').css('position', 'center ' + (wScroll) + 'px')
-// }
-
-
-// trying to copy https://www.youtube.com/watch?v=SmolT-tV5Lw&t=295s
-
-
-var elements = document.querySelectorAll('#pink_car');
 
 anime({
     targets: "#bizIntTitle",
-    translateX: 50
+    translateX: 50,
+    duration: 4000,                         // source: https://www.youtube.com/watch?v=f_gozrMnD7I
+    autoplay: false,
 });
 
 anime({
     targets: "#vendorTitle",
-    translateX: 50
+    translateX: 50,
+    duration: 4000,
+
 });
 
 anime({
     targets: "#opsTitle",
-    translateX: 50
+    translateX: 50,
+    duration: 4000,
 });
 
 anime({
     targets: "#auditTitle",
-    translateX: 50
+    translateX: 50,
+    duration: 4000,
+});
+
+
+const animation1 = anime({
+    targets: "#bizIntTitle",
+    translateX: 50,
+    duration: 4000,                         // source: https://www.youtube.com/watch?v=f_gozrMnD7I
+    autoplay: false,
+});
+
+
+// const section1 = document.querySelector("#office");
+
+// window.onscroll = function () {
+//     const scrollPercent = window.pageYOffset - section1.offsetTop;
+//     //console.log(scrollPercent);
+//     //console.log(window.pageYOffset);  //see how far away from the top
+//     console.log(section1.offsetTop);
+//     animation1.seek((scrollPercent / 100) * animation1.duration);
+// }
+
+
+
+var updates = 0;
+
+anime({
+    targets: '#box',
+    translateY: 270,
 });
