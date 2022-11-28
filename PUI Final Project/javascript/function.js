4ea8ee25964d813cc48af0292b2027e916603f96
liansara <97773028+liansara@users.noreply.github.com>
2022-11-27 19:58:20 -0500

    Overhual of visuals to the left

--- PUI Final Project/javascript/function.js
+++ PUI Final Project/javascript/function.js
@@ -1,11 +1,34 @@
-$(window).scroll(function () {
-    parallax();
-})
+// $(window).scroll(function () {
+//     parallax();
+// })
 
-function parallax() {
-    var wScroll = $(window).scrollTop();
-    $('.parallax--bg').css('position', 'center ' + (wScroll) + 'px')
-}
+// function parallax() {
+//     var wScroll = $(window).scrollTop();
+//     $('.parallax--bg').css('position', 'center ' + (wScroll) + 'px')
+// }
 
 
 // trying to copy https://www.youtube.com/watch?v=SmolT-tV5Lw&t=295s
+
+
+var elements = document.querySelectorAll('#pink_car');
+
+anime({
+    targets: "#bizIntTitle",
+    translateX: 20
+});
+
+anime({
+    targets: "#vendorTitle",
+    translateX: 20
+});
+
+anime({
+    targets: "#opsTitle",
+    translateX: 20
+});
+
+anime({
+    targets: "#auditTitle",
+    translateX: 20
+});
